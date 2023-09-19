2a25dff7108eab673d7d57a902d2f2fce190e7ac
Italo <soarescmpos@gmail.com>
2023-09-19 15:43:40 -0300

    first: Add home components

---
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -0,0 +1,24 @@
+import { Button, TextInput } from '@ignite-ui/react'
+import { Form } from './styles'
+import { ArrowRight } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+
+export function ClaimUsernameForm() {
+  const { register } = useForm()
+
+  return (
+    <Form as="form">
+      <TextInput
+        crossOrigin=""
+        size="sm"
+        prefix="ignite.com/"
+        placeholder="seu-usuário"
+        {...register('username')}
+      />
+      <Button size="sm" type="submit">
+        Reservar
+        <ArrowRight />
+      </Button>
+    </Form>
+  )
+}
